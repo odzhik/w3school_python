7c4a9ad0a07ea95a9942429a5b23f259b30b9b45
odzhik <zhakayev.o03@gmail.com>
2024-04-06 03:47:15 +0600

    initial commit

---
+++ w3school/if_else.py
@@ -0,0 +1,57 @@
+# a = 30
+# b = 20
+# if a > b:
+#     print("a is greater than b")
+
+# a = 33
+# b = 33
+# if b > a:
+#     print("b is greater than a")
+# elif a == b:
+#     print("a and b are equal")
+
+# a = 200
+# b = 33
+# if b > a:
+#     print("b is greater than a")
+# elif a == b:
+#     print("a and b are equal")
+# else:
+#     print("a is greater than b")
+
+# a = 200
+# b = 33
+# if b > a:
+#     print("b is greater than a")
+# else:
+#     print("b is not greater than a")
+
+a = 10
+b = 50
+c  = 10
+# if a > b: print("a is greater than b")
+
+# print("A") if a > b else print("B")
+
+# if a > b and c < a:
+#     print("A")
+
+# if a > b or c < a:
+#     print("A is greatest")
+
+# if not a > b:
+#     print("b is greater")
+
+x = 41
+
+# if x > 10:
+#     print("Above ten")
+#     if x > 20:
+#         print("above 20")
+#     else:
+#         print("but not above 20")
+
+a = 40
+b = 50
+if b > a:
+    pass
