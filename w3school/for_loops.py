5bbc33d8f71b3ad0baa246f16eaff0c87b74fffb
odzhik <zhakayev.o03@gmail.com>
2024-04-10 05:04:14 +0600

    new commit

---
+++ w3school/for_loops.py
@@ -0,0 +1,59 @@
+# cars = ["BMW", "Mercedes", "Audi", "VW"]
+# for x in cars:
+#     print(x)
+
+# for x in "banana":
+#     print(x)
+
+# cars = ["BMW", "Mercedes", "Audi", "VW"]
+# for x in cars:
+#     print(x)
+#     if x == "Audi": # BMW, Mercedes, Audi
+#         break
+
+# cars = ["BMW", "Merceds", "Audi", "VW"]
+# for x in cars:
+#     if x == "Audi":
+#         break
+#     print(x) # BMW, Mercedes
+
+# cars = ["Toyota", "Lexus", "Honda", "Nissan"]
+# for x in cars:
+#     if x == "Honda":
+#         continue
+#     print(x)
+
+# for x in range(6):
+#     print(x)
+
+# for x in range(2, 6): from 2 to 5: 2,3,4,5
+#     print(x)
+
+# for x in range(2, 30, 3): # from 2 to 30 with incremental 3: 2, 5, 8 ...
+#     print(x)
+
+# for x in range(6):
+#     print(x)
+# else:
+#     print("5 is maximum")
+
+# for x in range(10):
+#     if x == 3:
+#         break
+#     print(x) # 0, 1, 2. 3 is stopped, else condition doesnt work cause we stopped
+# else:
+#     print("3 is max")
+
+#Nested loops
+
+# cars = ["BMW", "Mercedes", "Audi", "VW"]
+# models = ["3 - series", "C- klasse", "A4", "Polo"]
+
+# for x in cars:
+#     for y in models:
+#         print(x, y) # X * Y
+
+for x in [0,1,2,3]:
+    pass
+
+
