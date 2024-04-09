d237007f5e422aff880f93e10736ecd2e6a7ba16
odzhik <zhakayev.o03@gmail.com>
2024-04-10 04:15:45 +0600

    new commit

---
+++ w3school/while_loops.py
@@ -0,0 +1,27 @@
+# i = 1 
+# while i < 6:
+#     print(i)
+#     i += 1 # i = i + 1
+
+# i = 1
+# while i < 6:
+#     print(i)
+#     if i == 5:
+#         break
+#     i += 1
+
+# i = 0
+# while i < 6:
+#     i = i + 1
+#     if i == 5:
+#         continue # skip 5: 1,2,3,4,6
+#     print(i)
+
+i = 1
+while i <= 6:
+    print(i)
+    i = i + 1
+else:
+    print("the max number is 6")
+
+
